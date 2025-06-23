9d134785476eaf21325d0c3b8ba26049f6a9a04b
YunQian <1121913691@qq.com>
2025-06-23 22:34:13 +0800

    xxx

---
+++ circuit.py
@@ -0,0 +1,8 @@
+import tensorcircuit as tc
+c = tc.Circuit(2)
+c.H(0)
+c.CNOT(0,1)
+c.rx(1, theta=0.2)
+print(c.wavefunction())
+print(c.expectation_ps(z=[0, 1]))
+print(c.sample(allow_state=True, batch=1024, format="count_dict_bin"))
